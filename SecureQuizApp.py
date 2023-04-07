5aa76b0a37620e2c7b0f3f039c5f71bda5d39b55
EllaW34 <ellaweathers3@gmail.com>
2023-04-07 13:49:34 -0700

    second upload

--- SecureQuizApp.py
+++ SecureQuizApp.py
@@ -12,30 +12,67 @@ app.secret_key=os.environ["SECRET_KEY"];
 def renderMain():
     return render_template('SecureQuizAppHome.html')
     
-@app.route('/page1')
+@app.route('/startOver')
+def startOver():
+    session.clear()
+    return redirect(url_for('renderMain'))
+    
+@app.route('/page1', methods=['GET', 'POST'])
 def renderPage1():
+    print("a1" in session)
+    if "a1" in session:
+        return redirect("/page2")
     return render_template('SecureQuizAppPage1.html')
     
-@app.route('/page2')
+@app.route('/page2', methods=['GET', 'POST'])
 def renderPage2():
+    if "a1" in request.form:
+        session["a1"]=request.form['a1']
+    print(session)
     return render_template('SecureQuizAppPage2.html')
     
-@app.route('/page3')
+@app.route('/page3', methods=['GET', 'POST'])
 def renderPage3():
+    session["a2"]=request.form['a2']
     return render_template('SecureQuizAppPage3.html')
     
-@app.route('/page4')
+@app.route('/page4', methods=['GET', 'POST'])
 def renderPage4():
+    session["a3"]=request.form['a3']
     return render_template('SecureQuizAppPage4.html')
     
-@app.route('/page5')
+@app.route('/page5', methods=['GET', 'POST'])
 def renderPage5():
+    session["a4"]=request.form['a4']
     return render_template('SecureQuizAppPage5.html')
     
-@app.route('/page6')
+@app.route('/page6', methods=['GET', 'POST'])
 def renderPage6():
-    return render_template('SecureQuizAppPage6.html')
+    session["a5"]=request.form['a5']
+    score = 0;
+    an1 = "Incorrect"
+    an2 = "Incorrect"
+    an3 = "Incorrect"
+    an4 = "Incorrect"
+    an5 = "Incorrect"
+    if(session['a1'] == "Their" or session['a1'] == "their"):
+        score += 1;
+        an1 = "Correct"
+    if(session['a2'] == "1.9" or session['a2'] == "less than 2" or session['a2'] == "Less than 2"):
+        score += 1;
+        an2 = "Correct"
+    if(session['a3'] == "Fleshy" or session['a3'] == "fleshy"):
+        score += 1;
+        an3 = "Correct"
+    if(session['a4'] == "Rhode Island" or session['a4'] == "rhode island" or session['a4'] == "Rhode island"):
+        score += 1;
+        an4 = "Correct"
+    if(session['a5'] == "True" or session['a5'] == "true"):
+        score += 1;
+        an5 = "Correct"
+    
+    return render_template('SecureQuizAppPage6.html', score = score, an1 = an1, an2 = an2, an3 = an3, an4 = an4, an5 = an5)
     
 
 if __name__=="__main__":
-    app.run(debug=False)
+    app.run(debug=True)
